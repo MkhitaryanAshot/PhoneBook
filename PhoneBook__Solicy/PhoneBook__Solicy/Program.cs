f7eba2541471b2267ecf8e362a5450714d3def7e
MkhitaryanAshot <101067241+MkhitaryanAshot@users.noreply.github.com>
2023-02-28 15:44:49 +0400

    Add files via upload

---
+++ PhoneBook__Solicy/PhoneBook__Solicy/Program.cs
@@ -0,0 +1,14 @@
+﻿using PhoneBook__Solicy;
+
+Console.WriteLine("Please Insert File Name");
+var fileName = Console.ReadLine();
+PhoneBook phoneBook = new PhoneBook(fileName);
+phoneBook.PrintMassages();
+
+if(PhoneBook.check)
+    phoneBook.Insertion();
+
+
+
+
+
